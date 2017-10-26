5de71cf78347db1113bab16a8d8d5a3eb109b799
SRC8775 <sanghamitra@bitcanny.com>
2017-10-26 18:43:19 +0530

    test should fail

--- __tests__/todo.test.js
+++ __tests__/todo.test.js
@@ -11,5 +11,5 @@ test('TodoComponent calls doneChange when todo is clicked', () => {
 
   const p = wrapper.find('.toggle-todo');
   p.simulate('click');
-  expect(doneChange).toBeCalledWith(1);
+  expect(doneChange).nottoBeCalledWith(1);
 });
